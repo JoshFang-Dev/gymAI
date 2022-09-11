7019b18a179305d9d37b8f14d31d21288be2af8c
JoshFang-Dev <joshuafang1998@gmail.com>
2022-09-11 10:39:27 -0700

    Initial Commit

---
+++ GymAI/GymAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAIApp.swift
+//  GymAI
+//
+//  Created by Josh Fang on 2022-09-11.
+//
+
+import SwiftUI
+
+@main
+struct GymAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
